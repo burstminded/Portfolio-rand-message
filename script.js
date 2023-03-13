8ce21affef9865ba564dc96179f2fdc8337e0d0a
burstminded <happytown236@gmail.com>
2023-03-13 10:11:19 +0200

    Add function to show message

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ for(let prop in adviceBox) {
     let messageIndex = randNum(adviceBox[prop].length);
 
     switch (prop) {
-        case langToLearn:
+        case 'langToLearn':
             messageBox.push(adviceBox[prop][messageIndex]);
             console.log(messageBox);
             break;
-        case levelOfKnowledge:
-            messageBox.push(messageIndex[prop][messageIndex]);
+        case 'levelOfKnowledge':
+            messageBox.push(adviceBox[prop][messageIndex]);
             messageBox.push(messageIndex + 2);
             console.log(messageBox);
             break;
-        case exampleProject:
+        case 'exampleProject':
             messageBox.push(adviceBox[prop][messageIndex]);
             console.log(messageBox);
             break;
@@ -29,3 +29,30 @@ for(let prop in adviceBox) {
             break;
     }
 }
+
+const finalMeesage = freeWeeks => {
+    let finMessage = [];
+    finMessage.push(`You have ${freeWeeks} free weeks`);
+    for (let i = 0; i < messageBox.length; i++) {
+        switch(i) {
+            case 0:
+                finMessage.push(`You can learn ${messageBox[i]}`);
+                break;
+            case 1:
+                finMessage.push(`We suggest you to learn it on ${messageBox[i]} level`);
+                break;
+            case 2:
+                finMessage.push(`You will need to spend at least ${messageBox[i]} hours learning a day`);
+                break;
+            case 3:
+                finMessage.push(`At the end of your learning course, you can make pet-project, ${messageBox[i]} for example`);
+                break;
+            default:
+                break;
+        }
+    }
+    let outputMessage = finMessage.join('.\n');
+    return outputMessage;
+}
+
+console.log(finalMeesage(5));
